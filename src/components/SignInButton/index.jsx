3b5fe1ad86e64b73da4f03773f8c904beb196974
Guilherme Regis <guilhermeb.regis@gmail.com>
2021-10-15 22:47:13 -0300

    authentication with spotify works :)

--- src/components/SignInButton/index.jsx
+++ src/components/SignInButton/index.jsx
@@ -7,21 +7,17 @@ import {signIn, signOut, useSession} from 'next-auth/client';
 
 export function SignInButton() {
     
-    console.log(process.env.NEXT_PUBLIC_CLIENT_ID);
-
-    const isUserLoggedIn = false;
-
     const [session, loading] = useSession();
-    console.log(session)
     
-    return isUserLoggedIn ? (
+    
+    return session ? (
         <button 
             type="button"
             className={styles.signInButton}
             onClick={signOut}
             >
             <FaSpotify color="white"/>
-            Guilherme Regis
+              {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (
