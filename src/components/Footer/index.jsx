4b7ad61f0d153c7c39d2ce6413e43fb135627c55
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-02-22 15:07:29 -0300

    fixes layout details

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,8 @@ export default function Footer() {
     <div className={styles.footer}>
       <p>
         Developed by
-        <a href="https://guilhermeregis.com" target="_blank" rel="noreferrer"> Guilherme Regis</a>
+        {' '}
+        <a href="https://guilhermeregis.com" target="_blank" rel="noreferrer">Guilherme Regis</a>
       </p>
     </div>
   );
