c8b2c384c1df6aec17d81c6565169379aa285462
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-02-24 23:37:28 -0300

    changes scopes

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,21 +5,11 @@ import SpotifyWebApi from 'spotify-web-api-node';
 
 // TODO remover escopos desnecessarios
 const scopes = [
-  'user-read-email',
-  'playlist-read-private',
-  'playlist-read-collaborative',
-  'playlist-modify-private',
+  // 'user-read-email',
+  // 'playlist-read-private',
+  // 'playlist-read-collaborative',
+  // 'playlist-modify-private',
   'playlist-modify-public',
-  'user-read-email',
-  'streaming',
-  'user-read-private',
-  'user-library-read',
-  'user-top-read',
-  'user-read-playback-state',
-  'user-modify-playback-state',
-  'user-read-currently-playing',
-  'user-read-recently-played',
-  'user-follow-read',
 ].join(',');
 
 const spotifyApi = new SpotifyWebApi({
